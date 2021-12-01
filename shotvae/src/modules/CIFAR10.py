e7e551adc4a99ea90f9b9df1bfe5043082dea675
an-seunghwan <dpeltms79@gmail.com>
2021-12-01 20:47:33 +0900

    update

---
+++ shotvae/src/modules/CIFAR10.py
@@ -0,0 +1,145 @@
+#%%
+import tensorflow as tf
+import tensorflow.keras as K
+from tensorflow.keras import layers
+#%%
+class BasicBlock(K.layers.Layer):
+    def __init__(self, 
+                 params,
+                 in_filter_size, 
+                 out_filter_size,
+                 strides, 
+                 slope=0.0,
+                 **kwargs):
+        super(BasicBlock, self).__init__(**kwargs)
+        self.params = params
+        
+        self.slope = slope
+        self.norm1 = layers.BatchNormalization()
+        self.conv1 = layers.Conv2D(filters=out_filter_size, kernel_size=3, strides=strides, 
+                                    padding='same', use_bias=False, kernel_regularizer=K.regularizers.l2(self.params['weight_decay']))
+        self.norm2 = layers.BatchNormalization()
+        self.conv2 = layers.Conv2D(filters=out_filter_size, kernel_size=3, strides=1, 
+                                    padding='same', use_bias=False, kernel_regularizer=K.regularizers.l2(self.params['weight_decay']))
+        
+        self.equalInOut = (in_filter_size == out_filter_size)
+        if not self.equalInOut:
+            self.conv3 = layers.Conv2D(filters=out_filter_size, kernel_size=1, strides=strides, 
+                                        padding='same', use_bias=False, kernel_regularizer=K.regularizers.l2(self.params['weight_decay']))
+
+    def call(self, x, training=True):
+        if not self.equalInOut:
+            x = tf.nn.leaky_relu(self.norm1(x, training=training), alpha=self.slope)
+            h = tf.nn.leaky_relu(self.norm2(self.conv1(x), training=training), alpha=self.slope)
+        else:
+            h = tf.nn.leaky_relu(self.norm1(x, training=training), alpha=self.slope)
+            h = tf.nn.leaky_relu(self.norm2(self.conv1(h), training=training), alpha=self.slope)
+        h = self.conv2(h)
+        if not self.equalInOut:
+            h = h + self.conv3(x)
+        else:
+            h = h + x
+        return h
+#%%
+class WideResNet(K.models.Model):
+    def __init__(self, params, name="WideResNet", **kwargs):
+        super(WideResNet, self).__init__(name=name, **kwargs)
+        self.params = params
+        
+        assert (self.params['depth'] - 4) % 6 == 0
+        self.block_depth = (self.params['depth'] - 4) // 6
+        self.nChannels = [16, 16*self.params['widen_factor'], 32*self.params['widen_factor'], 64*self.params['widen_factor']]
+        
+        # preprocess (small_input = True)
+        self.conv = layers.Conv2D(filters=self.nChannels[0], kernel_size=3, strides=1, 
+                                    padding='same', use_bias=False, kernel_regularizer=K.regularizers.l2(self.params['weight_decay']))
+        
+        # output: 32x32x32
+        self.block1 = K.models.Sequential([BasicBlock(self.params, self.nChannels[0], self.nChannels[1], strides=1, slope=self.params['slope'])] + \
+                                            [BasicBlock(self.params, self.nChannels[1], self.nChannels[1], strides=1, slope=self.params['slope'])])
+        
+        # output: 16x16x64
+        self.block2 = K.models.Sequential([BasicBlock(self.params, self.nChannels[1], self.nChannels[2], strides=2, slope=self.params['slope'])] + \
+                                            [BasicBlock(self.params, self.nChannels[2], self.nChannels[2], strides=1, slope=self.params['slope'])])
+        
+        # output: 8x8x128
+        self.block3 = K.models.Sequential([BasicBlock(self.params, self.nChannels[2], self.nChannels[3], strides=2, slope=self.params['slope'])] + \
+                                            [BasicBlock(self.params, self.nChannels[3], self.nChannels[3], strides=1, slope=self.params['slope'])])
+        
+        self.norm = layers.BatchNormalization()
+        
+    def call(self, x, training=True):
+        h = self.conv(x)
+        h = self.block1(h, training=training)
+        h = self.block2(h, training=training)
+        h = self.block3(h, training=training)
+        h = tf.nn.leaky_relu(self.norm(h, training=training), alpha=self.params['slope'])
+        h = layers.Flatten()(h)
+        return h
+#%%
+class Decoder(K.models.Model):
+    def __init__(self, params, name="Decoder", **kwargs):
+        super(Decoder, self).__init__(name=name, **kwargs)
+        self.params = params
+        self.num_feature = 64
+        self.model = []
+        dims = [self.num_feature * d for d in [16, 8, 4, 2]]
+        for i in range(len(dims)):
+            self.model.append(layers.Conv2DTranspose(filters = dims[i], kernel_size = 5, strides = 2, 
+                                                     padding = 'same', use_bias=False, kernel_regularizer=K.regularizers.l2(self.params['weight_decay'])))
+            self.model.append(layers.BatchNormalization())
+            self.model.append(layers.ReLU())
+        self.model.append(layers.Conv2DTranspose(filters = self.params['channel'], kernel_size = 4, strides = 2, 
+                                                 activation=self.params['activation'],
+                                                 padding = 'same', use_bias=False, kernel_regularizer=K.regularizers.l2(self.params['weight_decay'])))
+        
+    def call(self, x, training=True):
+        h = x[:, tf.newaxis, tf.newaxis, :]
+        for i in range(len(self.model)):
+            h = self.model[i](h, training=training)    
+        return h
+#%%
+class VAE(K.models.Model):
+    def __init__(self, params, name='VAE', **kwargs):
+        super(VAE, self).__init__(name=name, **kwargs)
+        self.params = params
+        
+        self.FeatureExtractor = WideResNet(self.params)
+        self.mean_layer = layers.Dense(self.params['latent_dim'], 
+                                       kernel_regularizer=K.regularizers.l2(self.params['weight_decay'])) 
+        self.logvar_layer = layers.Dense(self.params['latent_dim'], 
+                                         kernel_regularizer=K.regularizers.l2(self.params['weight_decay'])) 
+        self.prob_layer = layers.Dense(self.params['class_num'], activation='softmax', 
+                                       kernel_regularizer=K.regularizers.l2(self.params['weight_decay'])) 
+        self.Decoder = Decoder(self.params)
+        
+    def sample_gumbel(self, shape): 
+        U = tf.random.uniform(shape, minval=0, maxval=1)
+        return -tf.math.log(-tf.math.log(U + 1e-8) + 1e-8)
+
+    def gumbel_max_sample(self, probs): 
+        y = tf.math.log(probs + 1e-8) + self.sample_gumbel(tf.shape(probs))
+        y = tf.nn.softmax(y / self.params['temperature'])
+        if self.params['hard']:
+            y_hard = tf.cast(tf.equal(y, tf.math.reduce_max(y, 1, keepdims=True)), y.dtype)
+            y = tf.stop_gradient(y_hard - y) + y
+        return y
+    
+    def call(self, x, label=None, training=True):
+        h = self.FeatureExtractor(x, training=training)
+        mean = self.mean_layer(h)
+        logvar = self.logvar_layer(h)
+        if label is not None:
+            prob = label
+            '''NOT Gumbel-Softmax'''
+            y = label
+        else:
+            prob = self.prob_layer(h)
+            y = self.gumbel_max_sample(prob)
+        
+        noise = tf.random.normal((tf.shape(x)[0], self.params["latent_dim"]))
+        z = mean + tf.math.exp(logvar / 2) * noise 
+        
+        xhat = self.Decoder(tf.concat([z, y], axis=-1), training=training) 
+        return mean, logvar, prob, z, y, xhat
+#%%
---
+++ shotvae/src/train.py
@@ -0,0 +1,388 @@
+#%%
+import tensorflow as tf
+import tensorflow.keras as K
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+print('TensorFlow version:', tf.__version__)
+print('Eager Execution Mode:', tf.executing_eagerly())
+print('available GPU:', tf.config.list_physical_devices('GPU'))
+from tensorflow.python.client import device_lib
+print('==========================================')
+print(device_lib.list_local_devices())
+# tf.debugging.set_log_device_placement(False)
+#%%
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
+from datetime import datetime
+import sys
+import json
+import os
+os.chdir(r'D:\semi\shotvae')
+
+now = datetime.now() 
+date_time = now.strftime("%y%m%d_%H%M%S")
+
+from modules import CIFAR10
+#%%
+PARAMS = {
+    "data": 'cifar10',
+    "batch_size": 768,
+    "epochs": 600,
+    "data_dim": 32,
+    "channel": 3, 
+    "class_num": 10,
+    "labeled": 5000,
+    "temperature": 0.67,
+    "latent_dim": 128,
+    "sigma": 1.,
+    "annotated_ratio": 0.1,
+    
+    "kbmc": 1e-3, # kl beta max continuous
+    "kbmd": 1e-3, # kl beta max discrete
+    "akb": 200, # the max epoch to adjust kl beta
+    "ewm": 1e-3, # elbo weight max
+    "aew": 400, # the epoch to adjust elbo weight to max
+    "pwm": 1, # posterior weight max
+    "apw": 200, # adjust posterior weight
+    
+    "learning_rate": 0.1, 
+    "beta_1": 0.9, # beta_1 in SGD
+    "adjust_lr": [400, 500, 550], # the milestone list for adjust learning rate
+    "weight_decay": 5e-4 / 2, 
+    "epsilon": 0.1, # the label smoothing epsilon for labeled dataset
+    "activation": 'sigmoid',
+    "observation": 'bce',
+    
+    "hard": False,
+    "slope": 0.01, # pytorch default
+    "widen_factor": 2,
+    "depth": 28,
+}
+
+data_dir = r'D:\cifar10_{}'.format(PARAMS['labeled'])
+
+asset_path = 'weights_{}'.format(date_time)
+#%%
+'''triaining log file'''
+sys.stdout = open("./assets/{}/log_{}.txt".format(PARAMS['data'], date_time), "w")
+
+print(
+'''
+reproduce of SHOT-VAE
+'''
+)
+#%%
+model = CIFAR10.VAE(PARAMS) 
+#%%
+def weight_schedule(epoch, epochs, weight_max):
+    return weight_max * tf.math.exp(-5 * (1 - min(1, epoch/epochs)) ** 2)
+#%%
+'''dataset'''
+(x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
+
+if PARAMS['activation'] == 'tanh':
+    x_train = (x_train.astype('float32') - 127.5) / 127.5
+    x_test = (x_test.astype('float32') - 127.5) / 127.5
+elif PARAMS['activation'] == 'sigmoid':
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+else:
+    assert 0, "Unsupported observation model: {}".format(PARAMS['observation'])
+y_train_onehot = to_categorical(y_train, num_classes=PARAMS['class_num'])
+y_test_onehot = to_categorical(y_test, num_classes=PARAMS['class_num'])
+
+np.random.seed(1)
+'''ensure that all classes are balanced '''
+lidx = np.concatenate([np.random.choice(np.where(y_train == i)[0], 
+                                        int((len(x_train) * PARAMS['annotated_ratio']) / PARAMS['class_num']), 
+                                        replace=False) 
+                        for i in range(PARAMS['class_num'])])
+x_train_L = x_train[lidx]
+y_train_L = y_train_onehot[lidx]
+
+train_L_dataset = tf.data.Dataset.from_tensor_slices((x_train_L, y_train_L)).shuffle(len(x_train_L), reshuffle_each_iteration=True).batch(PARAMS['batch_size'])
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train)).shuffle(len(x_train), reshuffle_each_iteration=True).batch(PARAMS['batch_size'])
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(PARAMS['batch_size'])
+
+PARAMS['iterations'] = len(train_dataset)
+#%%
+with open('./assets/{}/params_{}.json'.format(PARAMS['data'], date_time), 'w') as f:
+    json.dump(PARAMS, f, indent=4, separators=(',', ': '))
+pprint(PARAMS)
+#%%
+def augmentation(image):
+    paddings = tf.constant([[0, 0],
+                            [4, 4],
+                            [4, 4],
+                            [0, 0]])
+    image = tf.pad(image, paddings, 'REFLECT')
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_crop(image, (image.shape[0], 32, 32, image.shape[-1]))
+    return image
+#%%
+def gaussian_kl_divergence(mean1, mean2, logvar1, logvar2):
+    return 0.5 * tf.reduce_sum(tf.square(mean1 - mean2) / tf.math.exp(logvar2)
+                               + tf.math.exp(logvar1) / tf.math.exp(logvar2) 
+                               - 1 
+                               + logvar2 
+                               - logvar1, axis=-1)
+#%%
+def test_cls_error(model, test_dataset):
+    '''test classification error''' 
+    error_count = 0
+    for x_batch, y_batch in test_dataset:
+        _, _, prob, _, _, _ = model(x_batch, training=False)
+        error_count += len(np.where(np.squeeze(y_batch) - np.argmax(prob.numpy(), axis=-1) != 0)[0])
+    return error_count / len(y_test)
+#%%
+@tf.function
+def supervised_train_step(x_batch_L, y_batch_L, PARAMS,
+                        ew, kl_beta_z, kl_beta_y, pwm, mix_weight,
+                        optimizer):
+    eps = 1e-8
+    
+    with tf.GradientTape(persistent=True) as tape:
+    
+        mean, logvar, prob, z, y, xhat = model(x_batch_L, y_batch_L)
+        
+        '''reconstruction'''
+        if PARAMS['observation'] == 'mse':
+            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.square(xhat - x_batch) / 2., axis=[1, 2, 3]))
+        elif PARAMS['observation'] == 'abs':
+            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.abs(xhat - x_batch), axis=[1, 2, 3]))
+        elif PARAMS['observation'] == 'bce':
+            recon_loss = tf.reduce_mean(tf.reduce_sum(x_batch_L * tf.math.log(xhat + eps) + 
+                                       (1. - x_batch_L) * tf.math.log(1 - xhat + eps), axis=[1, 2, 3]))
+        else:
+            assert 0, "Unsupported observation model: {}".format(PARAMS['observation'])
+            
+        '''prior: KL-divergence'''
+        kl_z = tf.reduce_mean(tf.reduce_sum(0.5 * (tf.square(mean) / PARAMS['sigma'] 
+                                                    + tf.math.exp(logvar) / PARAMS['sigma'] 
+                                                    + tf.math.log(PARAMS['sigma'])
+                                                    - logvar
+                                                    - 1), axis=-1))
+        kl_y = tf.reduce_mean(tf.reduce_sum(prob * tf.math.log(prob * PARAMS['class_num'] + 1e-8), axis=1))
+        elbo_loss_L = recon_loss + kl_beta_z * kl_z + kl_beta_y * kl_y
+        
+        '''mix-up'''
+        # no-gradient error!!!
+        # x_batch_L_shuffle = tf.random.shuffle(x_batch_L)
+        # y_batch_L_shuffle = tf.random.shuffle(y_batch_L)
+        # mean_shuffle = tf.random.shuffle(mean)
+        # logvar_shuffle = tf.random.shuffle(logvar)
+        x_batch_L_shuffle = tf.gather(x_batch_L, tf.random.shuffle(tf.range(x_batch_L.shape[0])))
+        y_batch_L_shuffle = tf.gather(y_batch_L, tf.random.shuffle(tf.range(y_batch_L.shape[0])))
+        mean_shuffle = tf.gather(mean, tf.random.shuffle(tf.range(mean.shape[0])))
+        logvar_shuffle = tf.gather(logvar, tf.random.shuffle(tf.range(logvar.shape[0])))
+        
+        x_batch_L_mix = mix_weight * x_batch_L_shuffle + (1 - mix_weight) * x_batch_L
+        mean_mix = mix_weight * mean_shuffle + (1 - mix_weight) * mean
+        var_mix = mix_weight * tf.sqrt(tf.math.exp(logvar_shuffle)) + (1 - mix_weight) * tf.sqrt(tf.math.exp(logvar))
+        smoothed_mean_mix, smoothed_logvar_mix, smoothed_prob_mix, _, _, _ = model(x_batch_L_mix)
+        
+        posterior_loss_z = tf.reduce_mean(tf.square(smoothed_mean_mix - mean_mix))
+        posterior_loss_z += tf.reduce_mean(tf.square(tf.sqrt(tf.math.exp(smoothed_logvar_mix)) - var_mix))
+        posterior_loss_y = tf.reduce_mean(mix_weight * tf.reduce_sum(y_batch_L_shuffle * tf.math.log(smoothed_prob_mix + eps), axis=-1))
+        posterior_loss_y += tf.reduce_mean((1. - mix_weight) * tf.reduce_sum(y_batch_L * tf.math.log(smoothed_prob_mix + eps), axis=-1))
+        
+        elbo_loss_L += kl_beta_z * pwm * posterior_loss_z
+        loss_supervised = ew * elbo_loss_L + posterior_loss_y
+            
+    grad = tape.gradient(loss_supervised, model.trainable_weights)
+    optimizer.apply_gradients(zip(grad, model.trainable_weights))
+    
+    return [[loss_supervised, recon_loss, kl_z, kl_y, posterior_loss_z, posterior_loss_y], 
+            [mean, logvar, prob, z, y, xhat]]
+#%%
+@tf.function
+def unsupervised_train_step(x_batch, x_batch_shuffle, mean_shuffle, logvar_shuffle, prob_shuffle, PARAMS,
+                        ew, kl_beta_z, kl_beta_y, pwm, mix_weight,
+                        optimizer):
+    eps = 1e-8
+    
+    with tf.GradientTape(persistent=True) as tape:
+        
+        mean, logvar, prob, z, y, xhat = model(x_batch)
+        
+        '''reconstruction'''
+        if PARAMS['observation'] == 'mse':
+            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.square(xhat - x_batch) / 2., axis=[1, 2, 3]))
+        elif PARAMS['observation'] == 'abs':
+            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.abs(xhat - x_batch), axis=[1, 2, 3]))
+        elif PARAMS['observation'] == 'bce':
+            recon_loss = tf.reduce_mean(tf.reduce_sum(x_batch_L * tf.math.log(xhat + eps) + 
+                                       (1. - x_batch_L) * tf.math.log(1 - xhat + eps), axis=[1, 2, 3]))
+        else:
+            assert 0, "Unsupported observation model: {}".format(PARAMS['observation'])
+            
+        '''prior: KL-divergence'''
+        kl_z = tf.reduce_mean(tf.reduce_sum(0.5 * (tf.square(mean) / PARAMS['sigma'] 
+                                                    + tf.math.exp(logvar) / PARAMS['sigma'] 
+                                                    + tf.math.log(PARAMS['sigma'])
+                                                    - logvar
+                                                    - 1), axis=-1))
+        kl_y = tf.reduce_mean(tf.reduce_sum(prob * tf.math.log(prob * PARAMS['class_num'] + 1e-8), axis=1))
+        elbo_loss_U = recon_loss + kl_beta_z * kl_z + kl_beta_y * kl_y
+        
+        '''mix-up'''
+        x_batch_mix = mix_weight * x_batch_shuffle + (1 - mix_weight) * x_batch
+        mean_mix = mix_weight * mean_shuffle + (1 - mix_weight) * mean
+        var_mix = mix_weight * tf.sqrt(tf.math.exp(logvar_shuffle)) + (1 - mix_weight) * tf.sqrt(tf.math.exp(logvar))
+        smoothed_mean_mix, smoothed_logvar_mix, smoothed_prob_mix, _, _, _ = model(x_batch_mix)
+        
+        posterior_loss_z = tf.reduce_mean(tf.square(smoothed_mean_mix - mean_mix))
+        posterior_loss_z += tf.reduce_mean(tf.square(tf.sqrt(tf.math.exp(smoothed_logvar_mix)) - var_mix))
+        pseudo_label = mix_weight * prob_shuffle + (1. - mix_weight) * prob
+        posterior_loss_y = tf.reduce_mean(tf.reduce_sum(pseudo_label * tf.math.log(smoothed_prob_mix + eps), axis=-1))
+        
+        elbo_loss_U += kl_beta_z * pwm * posterior_loss_z
+        loss_unsupervised = ew * elbo_loss_U + posterior_loss_y
+            
+    grad = tape.gradient(loss_unsupervised, model.trainable_weights)
+    optimizer.apply_gradients(zip(grad, model.trainable_weights))
+    
+    return [[loss_unsupervised, recon_loss, kl_z, kl_y, posterior_loss_z, posterior_loss_y], 
+            [mean, logvar, prob, z, y, xhat]]
+#%%
+# def generate_and_save_images(x, epochs):
+#     z = autoencoder.zEncoder(x, training=False)
+    
+#     plt.figure(figsize=(10, 2))
+#     plt.subplot(1, PARAMS['class_num']+1, 1)
+#     plt.imshow((x[0] + 1) / 2)
+#     plt.title('original')
+#     plt.axis('off')
+#     for i in range(PARAMS['class_num']):
+#         label = np.zeros((z.shape[0], PARAMS['class_num']))
+#         label[:, i] = 1
+#         xhat = autoencoder.Decoder(z, label, training=False)
+#         plt.subplot(1, PARAMS['class_num']+1, i+2)
+#         plt.imshow((xhat[0] + 1) / 2)
+#         plt.title('{}'.format(i))
+#         plt.axis('off')
+#     plt.savefig('./assets/{}/image_at_epoch_{}.png'.format(PARAMS['data'], epochs))
+#     # plt.show()
+#     plt.close()
+
+def generate_and_save_images(xhat, epoch):
+    xhat = xhat.numpy()
+    
+    plt.figure(figsize=(5, 5))
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.imshow((xhat[i] + 1) / 2)
+        plt.axis('off')
+    plt.savefig('./assets/{}/image_at_epoch_{}.png'.format(PARAMS['data'], epoch))
+    # plt.show()
+    plt.close()
+#%%
+test_error = [test_cls_error(model, test_dataset)] # initial test classification error
+
+for epoch in range(PARAMS['epochs']):
+    '''warm-up'''
+    if epoch == 0:
+        optimizer = K.optimizers.SGD(learning_rate=PARAMS['learning_rate'] * 0.2,
+                                    momentum=PARAMS['beta_1'])
+    
+    '''weights of loss terms'''
+    # elbo part weight
+    ew = weight_schedule(epoch, PARAMS['epochs'], PARAMS['ewm'])
+    # kl-divergence weight
+    kl_beta_z = weight_schedule(epoch, PARAMS['epochs'], PARAMS['kbmc'])
+    kl_beta_y = weight_schedule(epoch, PARAMS['epochs'], PARAMS['kbmd'])
+    # unsupervised classification weight
+    pwm = weight_schedule(epoch, PARAMS['epochs'], PARAMS['pwm'])
+        
+    step = 0
+    progress_bar = tqdm(range(PARAMS['iterations']))
+    progress_bar.set_description('iterations {}/{} | current loss ?'.format(step, PARAMS['iterations']))
+    
+    for _ in progress_bar:
+    
+        '''mini-batch'''
+        x_batch_L, y_batch_L = next(iter(train_L_dataset))
+        x_batch = next(iter(train_dataset))
+        
+        '''augmentation'''
+        x_batch_L = augmentation(x_batch_L)
+        x_batch = augmentation(x_batch)
+
+        '''mix-up weight'''
+        mix_weight = [tf.constant(np.random.beta(PARAMS['epsilon'], PARAMS['epsilon'])), 
+                      tf.constant(np.random.beta(PARAMS['epsilon'], PARAMS['epsilon']))]
+        
+        '''labeled dataset training'''
+        supervised_losses, supervised_outputs = supervised_train_step(x_batch_L, y_batch_L, PARAMS,
+                                                ew, kl_beta_z, kl_beta_y, pwm, mix_weight[0], optimizer) 
+        
+        '''mix-up: optimal match'''
+        mean, logvar, prob, _, _, _ = model(x_batch)
+        mean = mean.numpy()
+        logvar = logvar.numpy()
+        kl_metric = np.zeros((x_batch.shape[0], x_batch.shape[0]))
+        for i in range(x_batch.shape[0]):
+            kl_metric[i, :] = gaussian_kl_divergence(mean, mean[i],
+                                                    logvar, logvar[i]).numpy()
+        unsupervised_mix_up_index = np.argsort(kl_metric, axis=1)[:, 1]
+        x_batch_shuffle = tf.gather(x_batch, unsupervised_mix_up_index)
+        mean_shuffle = tf.gather(mean, unsupervised_mix_up_index)
+        logvar_shuffle = tf.gather(logvar, unsupervised_mix_up_index)
+        prob_shuffle = tf.gather(prob, unsupervised_mix_up_index)
+        
+        '''unlabeled dataset training'''
+        unsupervised_losses, unsupervised_outputs = unsupervised_train_step(x_batch, x_batch_shuffle, mean_shuffle, logvar_shuffle, prob_shuffle, PARAMS,
+                                                                            ew, kl_beta_z, kl_beta_y, pwm, mix_weight[1], optimizer)
+        
+        step += 1
+        
+        progress_bar.set_description('epoch: {} | iteration {}/{} | supervised {:.3f}, unsupervised {:.3f}, test {:.3f}'.format(
+            epoch, step, PARAMS['iterations'], 
+            supervised_losses[0].numpy(), unsupervised_losses[0].numpy(), test_error[-1]
+        )) 
+        
+        if step % 10 == 0:
+            print('epoch: {} | iteration {}/{} | supervised {:.3f}, unsupervised {:.3f}, test {:.3f}'.format(
+                epoch, step, PARAMS['iterations'], 
+                supervised_losses[0].numpy(), unsupervised_losses[0].numpy(), test_error[-1]
+            ))
+        
+    test_error.append(test_cls_error(model, test_dataset))
+        
+    if epoch % 50 == 0:
+        generate_and_save_images(unsupervised_outputs[-1], epoch)
+    
+    if epoch == 0:
+        learning_rate = K.optimizers.schedules.PiecewiseConstantDecay(
+            PARAMS['adjust_lr'], [PARAMS['learning_rate'] * t for t in [1., 0.1, 0.01, 0.001]]
+        )
+        optimizer = K.optimizers.SGD(learning_rate=learning_rate,
+                                    momentum=PARAMS['beta_1'])
+#%%
+'''save model'''
+# model.save_weights('./assets/{}/{}/weights'.format(PARAMS['data'], asset_path))
+os.makedirs('./assets/{}/{}'.format(PARAMS['data'], asset_path))
+model.save_weights('./assets/{}/{}/model.h5'.format(PARAMS['data'], asset_path), save_format="h5")
+model.summary()
+#%%
+'''import model'''
+# imported = CIFAR10.VAE(PARAMS)
+# dummy = tf.random.normal((1, 32, 32, 3))
+# '''call the model first'''
+# _ = imported(dummy)
+# imported.load_weights('./assets/{}/{}/model.h5'.format(PARAMS['data'], asset_path))
+# imported(x_batch_L)
+#%%
+'''test classification error phase'''
+plt.rc('xtick', labelsize=10)   
+plt.rc('ytick', labelsize=10)   
+fig, ax = plt.subplots(figsize=(15, 7))
+ax.plot(test_error, label='test classification error')
+leg = ax.legend(fontsize=15, loc='upper right')
+plt.savefig('./assets/{}/{}/classification_error_phase.png'.format(PARAMS['data'], asset_path),
+            dpi=200, bbox_inches="tight", pad_inches=0.1)
+# plt.show()
+plt.close()
+#%%
