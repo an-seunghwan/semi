2fac9c11637457b1188a3ebac9b025b2977666ef
an-seunghwan <dpeltms79@gmail.com>
2021-12-28 13:10:03 +0900

    Update main.py

--- sl/unconditional/main.py
+++ sl/unconditional/main.py
@@ -26,7 +26,7 @@ import datetime
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 from preprocess import fetch_dataset
-from model_separate import VAE
+from model import VAE
 # from criterion import ELBO_criterion
 from mixup import augment
 #%%
