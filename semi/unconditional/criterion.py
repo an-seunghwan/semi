1941e99ca0822f5fe080a3d90f3ddb05504768d7
an-seunghwan <dpeltms79@gmail.com>
2022-01-11 14:33:58 +0900

    update

--- semi/unconditional/criterion.py
+++ semi/unconditional/criterion.py
@@ -1,16 +1,26 @@
 #%%
 import tensorflow as tf
 #%%
-def ELBO_criterion(args, num_classes, x, xhat, mean, log_sigma, log_prob):
+def ELBO_criterion(args, x, xhatU, labelL, probL, nf_args):
         '''reconstruction'''
         if args['br']:
-            recon_loss = tf.reduce_mean(- tf.reduce_sum(x * tf.math.log(xhat) + 
-                                                        (1. - x) * tf.math.log(1. - xhat), axis=[1, 2, 3]))
+            recon_loss = tf.reduce_mean(- tf.reduce_sum(x * tf.math.log(tf.clip_by_value(xhatU, 1e-10, 1.0)) + 
+                                                        (1. - x) * tf.math.log(tf.clip_by_value(1. - xhatU, 1e-10, 1.0)), axis=[1, 2, 3]))
         else:
-            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.math.square(xhat - x) / (2. * (args['x_sigma'] ** 2)), axis=[1, 2, 3]))
+            recon_loss = tf.reduce_mean(tf.reduce_sum(tf.math.square(xhatU - x) / (2. * (args['x_sigma'] ** 2)), axis=[1, 2, 3]))
+        
+        '''classification'''
+        cls_loss = tf.reduce_mean(- tf.reduce_sum(labelL * tf.math.log(tf.clip_by_value(probL, 1e-10, 1.0)), axis=-1))
             
-        '''KL-divergence'''
-        kl_z = tf.reduce_mean(0.5 * tf.reduce_sum(tf.math.square(mean) + tf.math.exp(2. * log_sigma) - (2. * log_sigma) - 1, axis=-1))
-        kl_y = tf.reduce_mean(tf.reduce_sum(tf.math.exp(log_prob) * (log_prob - tf.math.log(1. / num_classes)), axis=1))
-        return recon_loss, kl_z, kl_y
+        # '''mutual information'''
+        # info = tf.reduce_mean(- tf.reduce_sum(probU * tf.math.log(tf.clip_by_value(prob_reconU, 1e-10, 1.0)), axis=-1))
+        
+        '''prior'''
+        z_nf_loss = tf.reduce_mean(tf.reduce_sum(tf.square(nf_args[0] - 0) / 2., axis=1))
+        z_nf_loss -= tf.reduce_mean(nf_args[1], axis=-1)
+        c_nf_loss = tf.reduce_mean(tf.reduce_sum(tf.square(nf_args[2] - 0) / 2., axis=1))
+        c_nf_loss -= tf.reduce_mean(nf_args[3], axis=-1)
+        nf_loss = z_nf_loss + c_nf_loss
+        
+        return recon_loss, cls_loss, nf_loss
 #%%
--- semi/unconditional/mixup.py
+++ semi/unconditional/mixup.py
@@ -1,7 +1,7 @@
 #%%
 from multiprocessing import cpu_count
 
-# import numpy as np
+import numpy as np
 import tensorflow as tf
 #%%
 @tf.function
@@ -14,53 +14,29 @@ def augment(x):
     x = tf.map_fn(lambda batch: tf.image.random_crop(batch, size=(32, 32, 3)), x, parallel_iterations=cpu_count())
     return x
 #%%
-# def gaussian_kl_divergence(mean1, mean2, log_sigma1, log_sigma2):
-#     return 0.5 * tf.reduce_sum(tf.math.square(mean1 - mean2) / tf.math.exp(2. * log_sigma2)
-#                                + tf.math.exp(2. * log_sigma1) / tf.math.exp(2. * log_sigma2) 
-#                                - 1 
-#                                + 2. * log_sigma2 
-#                                - 2. * log_sigma1, axis=-1)
-#%%
-# def label_smoothing(image, label, mean, log_sigma, mix_weight):
-#     # no-gradient error!!!
-#     # x_batch_L_shuffle = tf.random.shuffle(x_batch_L)
-#     # y_batch_L_shuffle = tf.random.shuffle(y_batch_L)
-#     # mean_shuffle = tf.random.shuffle(mean)
-#     # logvar_shuffle = tf.random.shuffle(logvar)
-#     image_shuffle = tf.gather(image, tf.random.shuffle(tf.range(tf.shape(image)[0])))
-#     label_shuffle = tf.gather(label, tf.random.shuffle(tf.range(tf.shape(label)[0])))
-#     mean_shuffle = tf.gather(mean, tf.random.shuffle(tf.range(tf.shape(mean)[0])))
-#     log_sigma_shuffle = tf.gather(log_sigma, tf.random.shuffle(tf.range(tf.shape(log_sigma)[0])))
+def label_smoothing(image, label, mix_weight, tag):
+    shuffled_indices = tf.random.shuffle(tf.range(start=0, limit=tf.shape(image)[0], dtype=tf.int32))
     
-#     image_mix = mix_weight * image_shuffle + (1. - mix_weight) * image
-#     mean_mix = mix_weight * mean_shuffle + (1. - mix_weight) * mean
-#     sigma_mix = mix_weight * tf.math.exp(log_sigma_shuffle) + (1. - mix_weight) * tf.math.exp(log_sigma)
+    image_shuffle = tf.gather(image, shuffled_indices)
     
-#     return image_mix, label_shuffle, mean_mix, sigma_mix
+    image_mix = mix_weight * image_shuffle + (1. - mix_weight) * image
+    if tag == 'labeled':
+        label_mix = tf.gather(label, shuffled_indices)
+    if tag == 'unlabeled':
+        label_shuffle = tf.gather(label, shuffled_indices)
+        label_mix = mix_weight * label_shuffle + (1. - mix_weight) * label
+    
+    return image_mix, label_mix
 #%%
-# def optimal_match_mix(image, mean, log_sigma, log_prob, mix_weight):
-#     kl_metric = np.zeros((tf.shape(mean)[0], tf.shape(mean)[0]))
-#     for i in range(tf.shape(mean)[0]):
-#         kl_metric[i, :] = gaussian_kl_divergence(mean, mean.numpy()[i], log_sigma, log_sigma.numpy()[i]).numpy()
+def weight_decay_decoupled(model, buffer_model, decay_rate):
+    # weight decay
+    for var, buffer_var in zip(model.variables, buffer_model.variables):
+        var.assign(var - decay_rate * buffer_var)
+    # update buffer model
+    for var, buffer_var in zip(model.variables, buffer_model.variables):
+        buffer_var.assign(var)
         
-#     unsupervised_mix_up_index = np.argsort(kl_metric, axis=1)[:, 1]
-#     image_shuffle = tf.gather(image, unsupervised_mix_up_index)
-#     mean_shuffle = tf.gather(mean, unsupervised_mix_up_index)
-#     log_sigma_shuffle = tf.gather(log_sigma, unsupervised_mix_up_index)
-#     log_prob_shuffle = tf.gather(log_prob, unsupervised_mix_up_index)
-    
-#     image_mix = mix_weight * image_shuffle + (1. - mix_weight) * image
-#     mean_mix = mix_weight * mean_shuffle + (1. - mix_weight) * mean
-#     sigma_mix = mix_weight * tf.math.exp(log_sigma_shuffle) + (1. - mix_weight) * tf.math.exp(log_sigma)
-#     pseudo_label = mix_weight * tf.math.exp(log_prob_shuffle) + (1. - mix_weight) * tf.math.exp(log_prob)
-    
-#     return image_mix, mean_mix, sigma_mix, pseudo_label
-#%%
-def weight_decay(model, decay_model, decay_rate):
-    for var, decay_var in zip(model.variables, decay_model.variables):
-        decay_var.assign(var - decay_rate * decay_var)
-            
-# def weight_decay(model, decay_rate):
-#     for var in model.trainable_variables:
-#         var.assign(var * (1 - decay_rate))
+def weight_decay(model, decay_rate):
+    for var in model.trainable_variables:
+        var.assign(var * (1. - decay_rate))
 #%%
