baf466a5891cc1bed4df353300c496a7ff4ab545
an-seunghwan <dpeltms79@gmail.com>
2022-02-13 15:37:54 +0900

    update

--- semi/proposal/result.py
+++ semi/proposal/result.py
@@ -160,7 +160,7 @@ log_path = f'logs/{args["dataset"]}_{args["labeled_examples"]}'
 
 datasetL, datasetU, val_dataset, test_dataset, num_classes = fetch_dataset(args, log_path)
 
-model_path = log_path + '/20220211-220455'
+model_path = log_path + '/20220211-221142'
 model_name = [x for x in os.listdir(model_path) if x.endswith('.h5')][0]
 model = VAE(args, num_classes)
 model.build(input_shape=(None, 32, 32, 3))
