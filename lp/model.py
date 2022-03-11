c121f9d9ebeac32629304f5d96ac3af0bb17cdc0
an-seunghwan <dpeltms79@gmail.com>
2022-03-11 18:17:25 +0900

    update

--- lp/model.py
+++ lp/model.py
@@ -48,15 +48,15 @@ class CNN(K.models.Model):
                 layers.MaxPool2D(pool_size=(2, 2), strides=2, padding='valid'),
                 layers.SpatialDropout2D(rate=0.5),
                 
-                tfa.layers.WeightNormalization(layers.Conv2D(filters=512, kernel_size=3, strides=1, padding='valid')),
+                tfa.layers.WeightNormalization(layers.Conv2D(filters=512, kernel_size=3, strides=1, padding='same')),
                 layers.BatchNormalization(),
                 layers.LeakyReLU(alpha=0.1),
                 
-                tfa.layers.WeightNormalization(layers.Conv2D(filters=256, kernel_size=1, strides=1, padding='valid')),
+                tfa.layers.WeightNormalization(layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same')),
                 layers.BatchNormalization(),
                 layers.LeakyReLU(alpha=0.1),
                 
-                tfa.layers.WeightNormalization(layers.Conv2D(filters=128, kernel_size=1, strides=1, padding='valid')),
+                tfa.layers.WeightNormalization(layers.Conv2D(filters=128, kernel_size=3, strides=1, padding='same')),
                 layers.BatchNormalization(),
                 layers.LeakyReLU(alpha=0.1),
                 
@@ -70,7 +70,8 @@ class CNN(K.models.Model):
         h = self.units(x, training=training)
         if self.isL2:
             h /= tf.math.maximum(tf.norm(h, axis=-1, keepdims=True), 1e-12)
-        return self.fc(h), h
+        logit = self.fc(h)
+        return logit, h
 #%%
 # model = CNN()
 # model.build(input_shape=(None, 32, 32, 3))
