a1c18dfa8862ffb00152695461f7877802430ade
an-seunghwan <dpeltms79@gmail.com>
2022-03-28 15:06:17 +0900

    Merge branch 'main' of https://github.com/an-seunghwan/semi

--- pi/main.py
+++ pi/main.py
@@ -2,8 +2,8 @@
 import argparse
 import os
 
-os.chdir(r'D:\semi\pi') # main directory (repository)
-# os.chdir('/home1/prof/jeon/an/semi/pi') # main directory (repository)
+# os.chdir(r'D:\semi\pi') # main directory (repository)
+os.chdir('/home1/prof/jeon/an/semi/pi') # main directory (repository)
 
 import numpy as np
 import tensorflow as tf
